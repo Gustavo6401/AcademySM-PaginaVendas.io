2ab61813cdb42b8b2c350155aa900d06ea8ad374
Gustavo6401 <gsoey2019123@gmail.com>
2023-09-27 17:57:45 -0300

    Página terminada, tudo graças ao grande ChatGPT.

--- js/Script.js
+++ js/Script.js
@@ -1,7 +1,16 @@
+// Menu Toggle
 const menuToggle = document.getElementById('menuToggle');
 const menu = document.getElementById('menu');
 
 menuToggle.addEventListener('click', () => {
     menu.classList.toggle('open');
     menu.classList.toggle('closed');
-})
+});
+
+// Abrir Formulário
+const botao = document.getElementById('botao');
+const formulario = document.getElementById('formulario');
+
+botao.addEventListener('click', () => {
+    formulario.style.display = 'flex';
+});
